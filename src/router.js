0a661ef5a14b8b23e3f12d36d09bb71d1f806665
Freki Managarm <mathieu.chambaud@ynov.com>
2021-02-23 11:57:12 +0100

    start pages and folders

---
+++ src/router.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+const router = () => {
+    return (
+        <Switch>
+            <Route component={} exact path="/" />
+        </Switch>
+    )
+}
+
+export default router
